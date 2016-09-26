1a7977d6f3167df29dd50838f654bb0a0b13b0e7
Mayank Solanki <mayankso@cybage.com>
2016-09-26 14:38:47 +0530

    added multiply, divide and subtract functionality

--- src/mynk/calc/Calculator.java
+++ src/mynk/calc/Calculator.java
@@ -4,4 +4,21 @@ public class Calculator {
 	public static double add(double num1,double num2){
 		return num1 + num2;
 	}
+	
+	public static double subtract(double num1,double num2){
+		return num1 - num2;
+	}
+
+	public static double multiply(double num1,double num2){
+		return num1 * num2;
+	}
+	
+	public static double divide(double num1,double num2) throws Exception{
+		if(num2 == 0.0){
+			throw new Exception("divide by zero");			
+		}
+		return num1 / num2;
+	}
+	
+	
 }
--- test/mynk/calc/test/CalculatorTest.java
+++ test/mynk/calc/test/CalculatorTest.java
@@ -14,4 +14,41 @@ public class CalculatorTest extends TestCase{
 		assertEquals(1.0,Calculator.add(-4, 5));
 		assertEquals(0.0,Calculator.add(4, -4));
 	}
+	
+	@Test
+	public void testMultiply(){
+		assertEquals(20.0,Calculator.multiply(4, 5));
+		assertEquals(-20.0,Calculator.multiply(4, -5));
+		assertEquals(-20.0,Calculator.multiply(-4, 5));
+		assertEquals(0.0,Calculator.multiply(4, 0));
+	}
+	
+	@Test
+	public void testSubtract(){
+		assertEquals(-1.0,Calculator.subtract(4, 5));
+		assertEquals(9.0,Calculator.subtract(4, -5));
+		assertEquals(-9.0,Calculator.subtract(-4, 5));
+		assertEquals(8.0,Calculator.subtract(4, -4));
+	}
+	
+	@Test
+	public void testDivide(){
+		try{
+			assertEquals(0.8,Calculator.divide(4, 5));
+			assertEquals(-0.8,Calculator.divide(4, -5));
+			assertEquals(-0.8,Calculator.divide(-4, 5));
+			assertEquals(-1.0,Calculator.divide(4, -4));
+		}
+		catch(Exception ex){
+			fail(ex.getMessage());
+		}
+		
+		try{
+			Calculator.divide(4, 0);
+			fail("Divide by zero should throw an exception");
+		}
+		catch(Exception ex){
+			
+		}
+	}
 }
